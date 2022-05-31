e0b8803a72e6ade1822f3201976eacc48de560eb
이건우 <lifeflavor@igeon-uui-MacBookPro.local>
2022-05-31 17:11:42 +0900

    create project

---
+++ src/store/about.js
@@ -0,0 +1,10 @@
+export default {
+     namespaced : true,
+     state : ()=> ({
+         name : 'rjsdnakfmzh',
+         email : 'rjsdnakfmzh@naver.com',
+         blog : 'blog~~~',
+         phone : '010-####-####',
+         image : 'https://heropy.blog/css/images/logo.png'
+     })
+}
